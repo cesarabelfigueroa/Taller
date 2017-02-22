c70256dd4e7e3b990091eedf9a9d7431d49f67e7
César Figueroa <cesar.figueroa@agilesolutions.com>
2017-02-22 13:30:24 -0600

    login and routes changes

--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.controller';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: '#/login', component: LoginComponent , pathMatch: 'full'},
+  { path: '#/home', component: HomeComponent , pathMatch: 'full'}
 ];
 
 export const routing = RouterModule.forRoot(routes);
