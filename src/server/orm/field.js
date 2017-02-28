5ef6c1884f856a818e5849d287e277eb671c1fc6
¨Herbert <herbertpaz13@outlook.com>
2017-02-27 21:34:37 -0600

    Constructor field

--- src/server/orm/field.js
+++ src/server/orm/field.js
@@ -1,7 +1,29 @@
 var Field = function (definition) {
+	if(!definition.hasOwnProperty("name") || !definition.hasOwnProperty("type")){
+		throw "Invalid Parameters";		
+	}	
+	if(typeof(definition.name) !== "string"){
+		throw "Invalid Name";
+	}
 	this.name = definition.name;
+	if (this.name.indexOf(" ") !== -1) {
+		this.name = "[" + this.name + "]";
+	}
 	this.type = definition.type;
+	this.isAutoIncrement = definition.isAutoIncrement;
+	if(definition.type == "string" || !definition.hasOwnProperty("isAutoIncrement")){
+		this.isAutoIncrement = false;
+	}	
 	this.dimension = definition.dimension;
+	if(!definition.hasOwnProperty("dimension")){
+		this.dimension = 45;	
+	}
 	this.defaultValue = definition.defaultValue;
-	this.options = definition.options;
+	this.options = JSON.parse(JSON.stringify(definition.options));
+	if(!definition.hasOwnProperty("options")){
+		this.options = null;
+	}
+	this.isPrimaryKey = definition.isPrimaryKey;
+	this.isForeignKey = definition.isForeignKey;
+	this.hasNull = definition.hasNull === undefined ? true : definition.hasNull;
 }
